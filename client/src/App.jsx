899a0c27bb7cb9bcecda2d25c6360a5e62c66b76
Naser-sk <nasir.shaikh.p9@gmail.com>
2024-08-17 13:00:56 +0530

    add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,34 @@
-import './App.css'
+import './App.css' 
+import Home from './components/Home'
+import Login from './components/Login'
+import SignUp from './components/SignUp'
 import Navbar from './components/Navbar'
 import About from './components/About'
-import Login from './components/Login'
-import Home from './components/Home'
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Products from './pages/Products'
+import AddProduct from './pages/AddProduct'
+import UpdateProduct from './pages/UpdateProduct'
+import Profile from './pages/Profile'
+
 function App() {
 
-  const router = createBrowserRouter([
-    {
-      path:"/",
-      element:<><Navbar/><Home/></>
-    },
-    {
-      path:"/login",
-      element:<><Navbar/><Login/></>
-    },
-    {
-      path:"about",
-      element:<><Navbar/><About/></>
-    }
-  ])
-
+ 
   return (
    <div>
-    <RouterProvider router={router}/>
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/about' element={<About/>}></Route>
+          <Route path='/login' element={<Login/>}></Route>
+          <Route path='/signup' element={<SignUp/>}></Route>
+          <Route path='/Product' element={<Products/>}></Route>
+          <Route path='/addProduct' element={<AddProduct/>}></Route>
+          <Route path='/updateProduct' element={<UpdateProduct/>}></Route>
+          <Route path='/profile' element={<Profile/>}></Route>
+        </Routes>
+      </BrowserRouter>
    </div>
   )
 }
