4540a3d2c02edfd6f0ec16dcdcdf3b31176d0d88
Naser-sk <nasir.shaikh.p9@gmail.com>
2024-08-18 11:31:14 +0530

    create signup and login page

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
-
+import './SignUp.css'
+import { Link } from 'react-router-dom'
 export default function Login() {
   return (
-    <div>Login</div>
+    <div className="signup-container">
+    <form className="signup-form">
+      <h2>Login</h2>
+      <div className="form-group">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          autoComplete='off'
+          placeholder='Enter your email'
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          autoComplete='off'
+          placeholder='Enter Password'
+        />
+      </div>
+      <button type="submit" className="submit-button">
+        Login
+      </button>
+      <div className='form-group'>
+      <p>Already have an account <Link to='/signup'>Sign Up</Link></p>
+      
+    </div>
+    </form>
+  </div>
   )
 }
--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,7 +1,49 @@
 import React from 'react'
-
+import './SignUp.css'
+import { Link } from 'react-router-dom'
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className="signup-container">
+    <form className="signup-form">
+      <h2>Sign Up</h2>
+      <div className="form-group">
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          autoComplete='off'
+          placeholder='Enter your name'
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          autoComplete='off'
+          placeholder='Enter your email'
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          autoComplete='off'
+          placeholder='Enter Password'
+        />
+      </div>
+      <button type="submit" className="submit-button">
+        Sign Up
+      </button>
+      <div className='form-group'>
+      <p>Already have an account <Link to='/login'>Login</Link></p>
+      
+    </div>
+    </form>
+  </div>
   )
 }
